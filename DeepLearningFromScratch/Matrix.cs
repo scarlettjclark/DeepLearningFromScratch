a3ae022eb64c0a36a9140e1e686c5ad26c579523
moehike <59832236+scarlettjclark@users.noreply.github.com>
2025-03-03 15:02:47 +0000

    finished implementing multiplication with tests

--- DeepLearningFromScratch/Matrix.cs
+++ DeepLearningFromScratch/Matrix.cs
@@ -1,4 +1,6 @@
-﻿namespace DeepLearningFromScratch
+﻿using System.Data;
+
+namespace DeepLearningFromScratch
 {
     public class Matrix
     {
@@ -28,13 +30,44 @@
 
         public static Matrix Multiply(Matrix matrix1, Matrix matrix2)
         {
-            if(matrix1._rows != matrix2._columns)
+            if(matrix1._columns != matrix2._rows)
             {
                 throw new ArgumentException("Matrices must have compatible sizes in order to multiply. Matrix 1 has " + matrix1._rows.ToString() + " rows but Matrix 2 has only " + matrix2._columns + " columns");
             }
             else
             {
-                return new(0, 0, []);
+                float[] outputValues = new float[matrix1._rows * matrix2._columns];
+                for (int row = 1; row <= matrix1._rows; row++)
+                {
+                    for (int column = 1; column <= matrix2._columns; column++)
+                    {
+                        outputValues[(row - 1) * matrix2._columns + (column - 1)] = 0;
+                        for(int index = 1; index <= matrix1._columns; index++)
+                        {
+                            outputValues[(row - 1) * matrix2._columns + (column - 1)] += matrix1.Access(row, index) * matrix2.Access(index, column);
+                        }
+                    }
+                }
+
+                return new Matrix(matrix1._rows, matrix2._columns, outputValues);
+
+            }
+        }
+
+        public bool Equals(Matrix other)
+        {
+            if (other._rows ==  _rows && other._columns == _columns)
+            {
+                bool result = true;
+                for (int index = 0; index < _storedValues.Length; index++)
+                {
+                    result &= _storedValues[index] == other._storedValues[index];
+                }
+                return result;
+            }
+            else
+            {
+                return false;
             }
         }
     }
--- Tests/MatrixTests.cs
+++ Tests/MatrixTests.cs
@@ -42,5 +42,65 @@ namespace DeepLearningFromScratch.Tests
 
             Assert.Throws<ArgumentException>(() => Matrix.Multiply(testMatrix1, testMatrix2));
         }
+        [Test]
+        public void Equals_MatricesWithSameDimensionAndSameValuesAreEqual_ReturnsTrue()
+        {
+            Matrix testMatrix1 = new(2, 2, [1, 0, 1, 1]);
+            Matrix testMatrix2 = new(2, 2, [1, 0, 1, 1]);
+            Assert.That(testMatrix1.Equals(testMatrix2), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Equals_MatricesWithDifferentDimensionsAreNotEqual_ReturnsFalse()
+        {
+            Matrix testMatrix1 = new(3, 2, [1, 0, 1, 1, 0, 1]);
+            Matrix testMatrix2 = new(2, 3, [1, 0, 1, 1, 0, 1]);
+
+            Assert.That(testMatrix1.Equals(testMatrix2), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Equals_MatricesWithDifferentValuesAreNotEqual_ReturnsFalse()
+        {
+            Matrix testMatrix1 = new(3, 2, [1, 0, 1, 1, 0, 1]);
+            Matrix testMatrix2 = new(3, 2, [1, 1, 0, 1, 0, 1]);
+
+            Assert.That(testMatrix1.Equals(testMatrix2), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Multiply_MultiplyingByTheIdentityMatrixReturnsTheSameMatrix_ReturnsCorrectMatrix()
+        {
+            Matrix matrix = new(2, 2, [1, 2, 3, 4]);
+            Matrix identity = new(2, 2, [1, 0, 0, 1]);
+            Matrix result = Matrix.Multiply(matrix, identity);
+
+            Assert.That(result.Equals(matrix), Is.EqualTo(true));
+        }
+
+        [TestCase(2, 2, (float[]) [1, 2, 3, 4], 2, 2, (float[]) [1, 2, 3, 4], 2, 2, (float[]) [7, 10, 15, 22])]
+        [TestCase(3, 2, (float[])[-1, 1, 1, -1, 1, 1], 2, 3, (float[])[1, -1, 1, -1, -1, -1], 3, 3, (float[])[-2, 0, -2, 2, 0, 2, 0, -2, 0])]
+        [TestCase(1, 3, (float[])[0.5f, 0.25f, 0.125f,], 3, 1, (float[])[-0.5f, -0.25f, -0.125f], 1, 1, (float[])[-0.328125f])]
+        public void Multiply_MultiplyingReturnsTheCorrectValues_ReturnsCorrectMatrix(
+                int rows1,
+                int columns1,
+                float[] values1,
+                int rows2,
+                int columns2,
+                float[] values2,
+                int expectedRows,
+                int expectedColumns,
+                float[] expectedValues
+            )
+        {
+            Matrix matrix1 = new(rows1, columns1, values1);
+            Matrix matrix2 = new(rows2, columns2, values2);
+            Matrix expectedResult = new(expectedRows, expectedColumns, expectedValues);
+
+            Matrix result = Matrix.Multiply(matrix1, matrix2);
+            Assert.That(result.Equals(expectedResult), Is.EqualTo(true));
+
+        }
+
     }
 }
