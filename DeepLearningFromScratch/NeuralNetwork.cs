26a4feedcbdfe331a7ce98c055f66ee060bf6b1a
moehike <59832236+scarlettjclark@users.noreply.github.com>
2025-03-04 16:43:20 +0000

    initial neural network tests

---
+++ DeepLearningFromScratch/NeuralNetwork.cs
@@ -0,0 +1,13 @@
+﻿namespace DeepLearningFromScratch
+{
+    public class NeuralNetwork
+    {
+        public NeuralNetwork(int layers, int[] layerSizes, Matrix[] weights)
+        {
+            if (layers != layerSizes.Length)
+            {
+                throw new ArgumentException("Neural network must declare the size of each of its layers. Has " + layers.ToString() + " layers, but only " + layerSizes.Length.ToString() + "sizes.");
+            }
+        }
+    }
+}
---
+++ Tests/NeuralNetworkTests.cs
@@ -0,0 +1,22 @@
+﻿using DeepLearningFromScratch;
+
+namespace Tests
+{
+    internal class NeuralNetworkTests
+    {
+        [Test]
+        public void NeuralNetwork_NeuralNetworkMustBeConstructedWithSpecifiedLayerSizes_ThrowsArgumentException()
+        {
+            Matrix weights = new Matrix(2, 2, [0.5f, 0.5f, 0.5f, 0.5f]);
+            Assert.Throws<ArgumentException>(() => new NeuralNetwork(3, [2, 2], [weights]));
+
+        }
+        /*[Test]
+        public void NeuralNetwork_NeuralNetworkMustBeConstructedWithCorrectlyShapedWeightMatrix_ThrowsArgumentException()
+        {
+            Matrix weights = new Matrix(0, 0, []);
+
+            Assert.Throws<ArgumentException>(() => new NeuralNetwork(2, [16, 16], [weights]));
+        }*/
+    }
+}
