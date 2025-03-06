28a72c3a3e7e9ba4fd63fe5dc276367bff5e553f
moehike <59832236+scarlettjclark@users.noreply.github.com>
2025-03-06 14:10:12 +0000

    new test for neural network initialisation

--- DeepLearningFromScratch/NeuralNetwork.cs
+++ DeepLearningFromScratch/NeuralNetwork.cs
@@ -8,6 +8,10 @@
             {
                 throw new ArgumentException("Neural network must declare the size of each of its layers. Has " + layers.ToString() + " layers, but only " + layerSizes.Length.ToString() + "sizes.");
             }
+            if (layers - 1 != weights.Length)
+            {
+                throw new ArgumentException("Neural network must have one less weight matrix than layers. Has " + layers.ToString() + " layers, but only " + weights.Length.ToString() + "weight matrices");
+            }
         }
     }
 }
--- Tests/NeuralNetworkTests.cs
+++ Tests/NeuralNetworkTests.cs
@@ -7,9 +7,16 @@ namespace Tests
         [Test]
         public void NeuralNetwork_NeuralNetworkMustBeConstructedWithSpecifiedLayerSizes_ThrowsArgumentException()
         {
-            Matrix weights = new Matrix(2, 2, [0.5f, 0.5f, 0.5f, 0.5f]);
+            Matrix weights = new(2, 2, [0.5f, 0.5f, 0.5f, 0.5f]);
             Assert.Throws<ArgumentException>(() => new NeuralNetwork(3, [2, 2], [weights]));
 
+        }
+        [Test]
+        public void NeuralNetwork_NeuralNetworkMustHaveWeightMatricesEqualToNumberOfLayersMinusOne_ThrowsArgumentException()
+        {
+            Matrix[] weights = [new Matrix(2, 2, [0.5f, 0.5f, 0.5f, 0.5f]),
+                                new Matrix (2, 2, [0.5f, 0.5f, 0.5f, 0.5f])];
+            Assert.Throws<ArgumentException>(() => new NeuralNetwork(4, [2, 2, 2, 2], weights));
         }
         /*[Test]
         public void NeuralNetwork_NeuralNetworkMustBeConstructedWithCorrectlyShapedWeightMatrix_ThrowsArgumentException()
